57b8d90a7144f2e164ff6c35c6376ca51c2470c1
ganesh-rajput-07 <grt7045@gmail.com>
2025-12-27 00:35:36 +0530

    1234

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
-python manage.py migrate
-gunicorn commandvault.wsgi:application
+#!/usr/bin/env bash
+set -e
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
